68b3e660b1232a4646a56cb926ef25455622260c
Max <tetraedrtrapecia@gmail.com>
2022-09-07 18:34:50 +0300

    6 ls finished

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Post, Tag
+from django.views.generic import View
+
+from .utils import ObjectDetailMixin
+
 
 # Create your views here.
 
 def posts_list(request):
-    n = ['Oleg', 'Masha', 'Olga', 'Ksu']
-    return render(request, 'blog/index.html', context={'names': n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
--- blogengine/urls.py
+++ blogengine/urls.py
@@ -21,5 +21,5 @@ from .views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog', include('blog.urls'))
+    path('blog/', include('blog.urls'))
 ]
